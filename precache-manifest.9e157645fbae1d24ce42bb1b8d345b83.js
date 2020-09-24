4ef215fd5d3ccc86d2fde999f7e3d202f1291cf6
Tareq <tareqtherocker@gmail.com>
2020-09-24 12:52:16 +0600

    Updates

---
+++ precache-manifest.9e157645fbae1d24ce42bb1b8d345b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e297e95b9e034d162d4188da52521dc",
+    "url": "/bari-koi-clone/index.html"
+  },
+  {
+    "revision": "9c1a5fed24d6620cf6b1",
+    "url": "/bari-koi-clone/static/css/main.3905d044.chunk.css"
+  },
+  {
+    "revision": "31df4ecbdb75e4e2037d",
+    "url": "/bari-koi-clone/static/js/2.678d5578.chunk.js"
+  },
+  {
+    "revision": "8819d044fc9cdbcc358fde839f0afde6",
+    "url": "/bari-koi-clone/static/js/2.678d5578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1a5fed24d6620cf6b1",
+    "url": "/bari-koi-clone/static/js/main.6936542d.chunk.js"
+  },
+  {
+    "revision": "d16d7b37031a5c3f4013",
+    "url": "/bari-koi-clone/static/js/runtime-main.d842cc5d.js"
+  }
+]);
